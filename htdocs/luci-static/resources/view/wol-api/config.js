ff0d8c9577c8be1cdd2168252316a245d627571d
root <root@localhost.localdomain>
2026-04-23 00:00:54 +0800

    feat: add etherwake import action in luci

--- htdocs/luci-static/resources/view/wol-api/config.js
+++ htdocs/luci-static/resources/view/wol-api/config.js
@@ -1,6 +1,7 @@
 'use strict';
 'require form';
 'require uci';
+'require ui';
 'require view';
 
 return view.extend({
@@ -52,6 +53,40 @@ return view.extend({
 		o.rmempty = false;
 		o.description = _('When enabled, API name lookup will also use targets from /etc/config/etherwake.');
 
+		o = s.option(form.Button, '_import_etherwake', _('Import etherwake targets into named devices'));
+		o.inputstyle = 'apply';
+		o.inputtitle = _('Import / Sync now');
+		o.onclick = function() {
+			var existing = {};
+
+			(uci.sections('wol-api', 'device') || []).forEach(function(dev) {
+				if (dev.name)
+					existing[dev.name] = dev['.name'];
+			});
+
+			(uci.sections('etherwake', 'target') || []).forEach(function(target) {
+				var sid;
+				if (!target.name || !target.mac)
+					return;
+
+				sid = existing[target.name];
+				if (!sid) {
+					sid = uci.add('wol-api', 'device');
+					existing[target.name] = sid;
+				}
+
+				uci.set('wol-api', sid, 'name', target.name);
+				uci.set('wol-api', sid, 'mac', target.mac);
+			});
+
+			uci.set('wol-api', 'main', 'use_etherwake_targets', '1');
+
+			return uci.save().then(function() {
+				ui.addNotification(null, E('p', _('Imported etherwake targets into named devices. Reloading page…')));
+				return location.reload();
+			});
+		};
+
 		o = s.option(form.DummyValue, '_etherwake_targets', _('Existing etherwake targets'));
 		o.rawhtml = true;
 		o.cfgvalue = function() {
@@ -61,6 +96,7 @@ return view.extend({
 		s = m.section(form.GridSection, 'device', _('Named devices'));
 		s.anonymous = true;
 		s.addremove = true;
+		s.description = _('These are the names and MACs used directly by the API. You can import them from luci-app-wol / etherwake with the button above.');
 
 		o = s.option(form.Value, 'name', _('Name'));
 		o.rmempty = false;
